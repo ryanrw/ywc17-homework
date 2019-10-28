d30fd4fd3d1338b9de59a47c4e1957a8ecc21b8b
Ryan-Willpower <surasak.c.work@gmail.com>
2019-10-29 00:21:55 +0700

    [refactor] remove @media rule due to mobile first

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-import { browserQueryRule } from '../utils'
-
 const ContainerMain = styled.div`
-  ${browserQueryRule.mobile} {
-    margin: 0 30px;
-  }
+  margin: 0 30px;
 `
 
 export const Container: React.FC = props => {
