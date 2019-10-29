39efd8f1a26d5fc9d6422596d22757e603a5ec9c
Ryan-Willpower <surasak.c.work@gmail.com>
2019-10-29 15:50:36 +0700

    [container] decrease margin

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 const ContainerMain = styled.div`
-  margin: 0 30px;
+  margin: 0 20px;
 `
 
 export const Container: React.FC = props => {
